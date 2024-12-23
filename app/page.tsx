5547b8fa6a19499de65362a24e5715834ca2b3b6
gerryhubai <geremaineemefa@gmail.com>
2024-12-23 04:17:40 +0000

    Update page.tsx to use my manifest

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@
 import MintSBT from "@/components/MintSBT";
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
-const MANIFEST_URL = 'https://gateway.pinata.cloud/ipfs/QmZd2pBynX8ieaBJHuEhkMQD9fCvCboKJsbEbpVJ7TLAEr';
+const MANIFEST_URL = 'https://gray-accused-harrier-397.mypinata.cloud/ipfs/bafkreigcw6dmtntan4rn2eorbyencyeg6mjd7nrz7ioxpvurel26zcwgjy';
 
 export default function Home() {
   return (
